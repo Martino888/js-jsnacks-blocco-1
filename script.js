7cf5602ad16ab581b656c56e921b424ebf08ec13
Martino888 <maxmartino33@gmail.com>
2022-02-25 14:54:24 +0100

    js-jsnacks-blocco-1

---
+++ script.js
@@ -0,0 +1,92 @@
+/*
+Snack 1
+L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.
+*/
+
+
+
+
+
+/*
+snack 2
+l' utente inserisce due parole in successione, con due prompt.
+il software stampa prima la parola piu corta, poi la parola piu lunga.
+*/
+
+/*
+const prima = prompt ("dammi una parola")
+const seconda = prompt ("dammi una seconda parola")
+const risultato = document.querySelector ('.parola')
+
+if( prima.length > seconda.length){
+    risultato.innerHTML = `${prima} <br> ${seconda}`
+}else if(prima.length < seconda.length){
+    risultato.innerHTML = seconda + ' ' + prima
+}else{
+    risultato.innerHTML = uguali
+}
+
+*/
+
+
+
+/*
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+*/
+
+
+let arrNum = [];
+
+for (i = 0; i < 10; i++) {
+    let userNum = parseInt(prompt("inserisci numero"));
+    arrNum.push(userNum);    
+}
+
+let somma = 0;
+for (i = 0; i < arrNum.length; i++){
+    somma = somma + arrNum[i];
+}  
+alert(somma);
+
+
+
+/* 
+Snack 3
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+*/
+
+
+
+
+
+/*
+Snack 4
+In un array sono contenuti i nomi degli invitati alla festa del grande
+Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o
+no alla festa.
+*/
+
+
+
+
+
+/* 
+Snack 5
+Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array.
+*/
+
+
+
+
+
+
+/*
+Snack 6
+Chiedi un numero di 4 cifre all’utente
+e calcola la somma di tutte le cifre che compongono il numero.
+*/
